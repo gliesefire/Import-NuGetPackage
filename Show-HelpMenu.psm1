8b89db4907cb12799703144dc8efa88d565006ec
gliesefire <74833347+gliesefire@users.noreply.github.com>
2024-01-23 03:32:48 +0530

    Updated docs (#3)

--- Show-HelpMenu.psm1
+++ Show-HelpMenu.psm1
@@ -1,46 +1,180 @@
-# Courtesy of https://github.com/danielbohannon/Invoke-Obfuscation/blob/master/Invoke-Obfuscation.ps1
+using namespace System;
+using namespace System.Collections.Generic;
+using namespace System.Text.RegularExpressions;
+using namespace System.IO;
 
 function Show-HelpMenu {
     # Input options to display non-interactive menus or perform actions.
-    $MenuInputOptionsShowHelp = @(@('help', 'get-help', '?', '-?', '/?', 'menu'), "Show this <Help> Menu                     `t  " )
-    $PackageNameOptionsShowOptions = @(@('package-name', 'name')       , "<Show options> for payload to obfuscate   `t  " )
-    $PackageVersionInputOptions = @(@('package-version', 'version')            , "<Clear> screen                            `t  " )
-    
-    # Add all above input options lists to be displayed in SHOW OPTIONS menu.
-    $AllAvailableInputOptionsLists = @()
-    $AllAvailableInputOptionsLists += , $MenuInputOptionsShowHelp
-    $AllAvailableInputOptionsLists += , $PackageNameOptionsShowOptions
-    $AllAvailableInputOptionsLists += , $PackageVersionInputOptions
+    $regexOptions = [RegexOptions]::Multiline -bor [RegexOptions]::IgnoreCase -bor [RegexOptions]::Compiled -bor [RegexOptions]::Global
+    $parameterParseRegex = [Regex]::new('(?:\[Alias\((?<aliases>[^\)]*)\)\])?[\r\n\t\s]*\[Parameter\([\r\n\t\s]*(?:Mandatory = \$(?:(?:\bfalse\b)|(?:\btrue\b))),[\r\n\t\s]*HelpMessage = "(?<helpMessage>[^"]*)"[\r\n\t\s]*\)\][\r\n\t\s]*\[[a-zA-Z]+\][\r\n\t\s]*\$(?<variableName>[a-zA-Z]+)', $regexOptions);
 
-    # Show Help Menu.
-    Write-Host "`n`nHELP MENU" -NoNewLine -ForegroundColor Cyan
-    Write-Host " :: Available" -NoNewLine
-    Write-Host " options" -NoNewLine -ForegroundColor Yellow
-    Write-Host " shown below:`n"
-    ForEach ($InputOptionsList in $AllAvailableInputOptionsLists) {
-        $InputOptionsCommands = $InputOptionsList[0]
-        $InputOptionsDescription = $InputOptionsList[1]
+    # Read the script itself and parse all the options
+    $script = [IO.File]::ReadAllText("./Import-NuGetPackage.psm1");
+    $regexMatches = $parameterParseRegex.Matches($script);
 
-        # Add additional coloring to string encapsulated by <> if it exists in $InputOptionsDescription.
-        If ($InputOptionsDescription.Contains('<') -AND $InputOptionsDescription.Contains('>')) {
-            $FirstPart = $InputOptionsDescription.SubString(0, $InputOptionsDescription.IndexOf('<'))
-            $MiddlePart = $InputOptionsDescription.SubString($FirstPart.Length + 1)
-            $MiddlePart = $MiddlePart.SubString(0, $MiddlePart.IndexOf('>'))
-            $LastPart = $InputOptionsDescription.SubString($FirstPart.Length + $MiddlePart.Length + 2)
-            Write-Host "$LineSpacing $FirstPart" -NoNewLine
-            Write-Host $MiddlePart -NoNewLine -ForegroundColor Cyan
-            Write-Host $LastPart -NoNewLine
+    # Create a list of all the options
+    $AllOptions = @();
+
+    ForEach ($match in $regexMatches) {
+        $variableName = $match.Groups['variableName'].Value;
+        $aliasMatch = $match.Groups['aliases'].Value;
+        if ([string]::IsNullOrWhiteSpace($aliasMatch)) {
+            $aliasMatch = $variableName;
         }
-        Else {
-            Write-Host "$LineSpacing $InputOptionsDescription" -NoNewLine
+        else
+        {
+            $aliasMatch = $aliasMatch + ", '$variableName'";
         }
-        
-        $Counter = 0
-        ForEach ($Command in $InputOptionsCommands) {
-            $Counter++
-            Write-Host $Command.ToUpper() -NoNewLine -ForegroundColor Yellow
-            If ($Counter -lt $InputOptionsCommands.Count) { Write-Host ',' -NoNewLine }
+
+        $aliasMatch = $aliasMatch.ToUpper().Trim();
+
+        $helpMessage = $match.Groups['helpMessage'].Value;
+
+        # if help message is more than 50 characters, wrap it to the next line
+        $helpMessage = Convert-TextToWrappedLines $helpMessage 70 $true
+        if ($helpMessage.GetType().Name -eq 'String') {
+            $x = [List[string]]::new();
+            $x.Add($helpMessage);
+            $helpMessage = $x;
         }
-        Write-Host ''
+
+        $aliasLines = Convert-TextToWrappedLines $aliasMatch 50 $false
+
+        if ($aliasLines.GetType().Name -eq 'String') {
+            $x = [List[string]]::new();
+            $x.Add($aliasLines);
+            $aliasLines = $x;
+        }
+
+        $AllOptions += , @($aliasLines, $helpMessage);
     }
+
+    WriteToConsoleWithColor -text "`n`nHELP MENU" -color $([ConsoleColor]::Cyan)
+    WriteToConsoleWithColor -text " :: Available" -insertNewLine $false
+    WriteToConsoleWithColor -text " options" -color $([ConsoleColor]::Yellow) -insertNewLine $false
+    WriteToConsoleWithColor -text " shown below:`n"
+
+    ForEach ($options in $AllOptions) {
+        $aliases = $options[0]
+        $helpMessage = $options[1]
+
+        $maxLines = [Math]::Max($aliases.Count, $helpMessage.Count);
+
+        for ($i = 0; $i -lt $maxLines; $i++) {
+
+            $line = ""
+            
+            if ($i -lt $helpMessage.Count) {
+                $line += $helpMessage[$i] + "   `t"
+            }
+
+            WriteToConsoleWithColor -text $line -insertNewLine $false
+            $line = ""
+            if ($i -lt $aliases.Count) {
+                $currentAlias = $aliases[$i]
+                $line += $currentAlias
+            }
+
+            WriteCodeToConsole -text $line
+        }
+    }
+}
+
+function Convert-TextToWrappedLines {
+    param(
+        [Parameter()]
+        [string]
+        $text,
+
+        [Parameter()]
+        [int]
+        $maxLineLength,
+
+        [Parameter()]
+        [bool]
+        $addIndentation = $true
+    )
+
+    $words = $text.Split(' ');
+    $wrappedLines = [List[string]]::new();
+
+    for ($i = 0; $i -lt $words.Length; $i++) {
+        $firstWord = $words[$i];
+        if ($addIndentation) {
+            if ($i -eq 0) {
+                $currentLine = "[*]`t$firstWord";
+            }
+            else {
+                $currentLine = "   `t$firstWord";
+            }
+        }
+        else {
+            $currentLine = $firstWord;
+        }
+
+        $currentLineLength = $currentLine.Length;
+
+        while ($i + 1 -lt $words.Length -and $currentLineLength + $words[$i + 1].Length + 1 -le $maxLineLength) {
+            $trimmedWord = $words[$i + 1].Trim();
+            if ($trimmedWord.Length -eq 0)
+            {
+                $i++;
+                continue;
+            }
+
+            $i++;
+            $currentLine += " " + $trimmedWord;
+            $currentLineLength = $currentLine.Length;
+        }
+
+        if ($addIndentation) {
+            $currentLine = [string]::Format("{0,-$maxLineLength}", $currentLine);
+        }
+
+        $wrappedLines.Add($currentLine);
+    }
+
+    return $wrappedLines;
+}
+
+function WriteCodeToConsole {
+    param(
+        [Parameter()]
+        [string]
+        $text,
+
+        [Parameter()]
+        [bool]
+        $insertNewLine = $true
+    )
+
+    WriteToConsoleWithColor -text $text -color $([ConsoleColor]::Yellow) -insertNewLine $insertNewLine
+}
+
+function WriteToConsoleWithColor {
+    param(
+        [Parameter()]
+        [string]
+        $text,
+
+        [Parameter()]
+        [ConsoleColor]
+        $color = [ConsoleColor]::White,
+
+        [Parameter()]
+        [bool]
+        $insertNewLine = $true
+    )
+
+    $oldForegroundColor = [Console]::ForegroundColor;
+    [Console]::ForegroundColor = $color;
+
+    if ($insertNewLine) {
+        [Console]::WriteLine($text);
+    }
+    else {
+        [Console]::Write($text);
+    }
+
+    [Console]::ForegroundColor = $oldForegroundColor;
 }
